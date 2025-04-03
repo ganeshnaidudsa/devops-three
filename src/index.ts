2dad86ede0323d7e76eb8892851a45375aa0edad
Ganesh Naidu <ganeshnaidudsa@gmail.com>
2025-04-03 21:42:26 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ app.get("/", (req : Request,res : Response) =>{
 
 app.get("/hello",(req : Request,res : Response)=>{
     res.json({
-        message : "First deployed"
+        message : "Hello from server !"
     })
 })
 
